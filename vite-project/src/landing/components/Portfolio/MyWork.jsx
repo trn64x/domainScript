de7b6b5d401f2ef8f5dc3554016c49cc67544606
trn64x <maciekp371@gmail.com>
2025-03-29 20:56:44 +0100

    update

--- vite-project/src/landing/components/Portfolio/MyWork.jsx
+++ vite-project/src/landing/components/Portfolio/MyWork.jsx
@@ -10,7 +10,7 @@ export default function MyWork() {
         {
             name: "Blackjack",
             date: "2025-03-25",
-            description:"React project of well known BlackJack",
+            description:"React project of well known BlackJack card game",
             link:"https://blackjack.mpotrz.pl",
             featured: true,
             language: "React"
@@ -20,12 +20,12 @@ export default function MyWork() {
             date: "2024-06-10",
             description: "simple to-do app made at begining of my journey",
             link: "https://trn64x.github.io/to-do-List/todo.html",
-            featured: false,
+            featured: true,
             language: "React"
         },
         {
             name: "Object Doors",
-            date: "2025-07-26",
+            date: "2025-03-26",
             description:"I was playing around with object javascript in React",
             link:"https://objectdoors.maciekp371.workers.dev",
             featured: true,
@@ -33,10 +33,10 @@ export default function MyWork() {
         },
         {
             name:"Python Graph",
-            date: "2025-08-25",
+            date: "2025-03-25",
             description:"Ever was wondering about powers on numbers? didn't too",
             link:"https://github.com/trn64x/PythonGraph",
-            featured: true,
+            featured: false,
             language: "Python"
         },
         {
@@ -51,7 +51,7 @@ export default function MyWork() {
             name:"Choinka",
             date:"2024-11-24",
             description:"funny project for mr. Soppa before christmas",
-            featured:false,
+            featured:true,
             language:"Javascript"
         }
 
@@ -59,6 +59,7 @@ export default function MyWork() {
     const [search, setSearch] = useState("");
     const [sortOrder, setSortOrder] = useState("date");
     const[selectedLanguage, setSelectedLanguage] = useState("any");
+    const[isFeatured, setisFeatured] = useState(null);
 
     // Filtering projects based on search
     let filteredProjects = projects.filter(project =>
@@ -68,6 +69,10 @@ export default function MyWork() {
         console.log(selectedLanguage);
         filteredProjects = filteredProjects.filter( project=> project.language === selectedLanguage)
     }
+    if(isFeatured !== null){
+        console.log(isFeatured);
+        filteredProjects = filteredProjects.filter( project=> project.featured === isFeatured) 
+    } 
     // Sorting projects by date
     filteredProjects.sort((a, b) => {
         if(sortOrder === "date") return
@@ -100,6 +105,11 @@ export default function MyWork() {
                     <option value="Typescript">Typescript</option>
                     <option value="Javascript">Javascript</option>
                 </select>
+                <select onChange={(e) => {setisFeatured(e.target.value === "any" ? null : e.target.value === "true")}}>
+                    <option value="any">Type</option>
+                    <option value={true}>Active</option>
+                    <option value={false}>Repository</option>
+                </select>
                 </div>
                 <h1>Contact me</h1>
                 <div className="containerfilters">
@@ -108,7 +118,7 @@ export default function MyWork() {
             </div>
 
              <motion.div className="flex"
-        initial={{scale:0.5,visibility:"hidden"}}
+        initial={{scale:0.9,visibility:"hidden"}}
         transition={{duration:0.75, ease:[0,0.71,0.2,1.01]}}
         whileInView={
             {scale:1, visibility:"visible"}
@@ -118,7 +128,7 @@ export default function MyWork() {
                 {filteredProjects.map((project, index) => (
                     <div key={index} className="item">
                         <div className="l">
-                        <p id="a2"><FontAwesomeIcon icon={faCalendar} flip="horizontal" style={{ marginRight:"10px",color: "#00FF99" }} />{project.date}</p>
+                        <p id="a2"><span><FontAwesomeIcon icon={faCalendar} flip="horizontal" style={{ marginRight:"10px",color: "#00FF99" }} />{project.date}</span><span style={project.featured ? {fontSize: "20px",color:"#00FF99"} : {fontSize: "20px",color:"#80FE01"}}>{project.featured ? "Active" : "Repository"}</span></p>
                         <p id= "a1"><FontAwesomeIcon icon={faPaperPlane} style={{marginRight:"10px", color: "#00FF99",}} />{project.name}</p>
                         <p id="a3">{project.description}</p>
                         </div>
--- vite-project/src/landing/components/header/header.jsx
+++ vite-project/src/landing/components/header/header.jsx
@@ -11,9 +11,9 @@ const [active, setActive] = useState("About me");
             <div id="logo">mpotrz.pl</div>
             <div id="socials"><a href="https://www.linkedin.com/in/maciekp-potrz-ba6a0b308/"><img id="link" src={logo}></img></a><a href="mailto:maciekp371@gmail.com"><img id="gmail" src= {gmail}></img></a><a href="https://github.com/trn64x"><img id="github" src= {github}></img></a> </div>
             <div id="options">
-            <a href="#root" className={ active === "About me" ? "active" : ""} onClick={() => {setActive("About me");}} >About me</a>
-            <a href="#Portfolio" className = {active === "My Work" ? "active" : ""} onClick={() => setActive("My Work")} >My work</a>
-            <a href="#Blog" className = {active === "Blog" ? "active" : ""} onClick={() => setActive("Blog")} >Blog</a></div>
+            <a href="#root" className= { active === "About me" ? "class" : "class"} onClick={() => {setActive("About me");}} >About me</a>
+            <a href="#Portfolio" className = {active === "My Work" ? "class" : "class"} onClick={() => setActive("My Work")} >My work</a>
+            <a href="#Blog" className = {active === "Blog" ? "class" : "class"} onClick={() => setActive("Blog")} >Blog</a></div>
         </header>
         </>
     );
